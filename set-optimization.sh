f594fd16ee86a256205d0091adad47e1908c6b76
cloudyluna <luna.cloudberry@proton.me>
2025-01-22 22:43:22 +0800

    Add optimization script change

---
+++ set-optimization.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+LEVEL="$1"
+MAKEFILE_VARS="$2"
+
+if [[ -z "$MAKEFILE_VARS" ]]
+then
+    echo "Need path to Makefile.common for second argument"
+    exit 1
+fi
+
+DEBUG_PAT='s/OPTIMIZE_FLAG = .*/OPTIMIZE_FLAG = ${OPTIMIZE_OFF_FLAG}/'
+DEBUG_NO_WARN_PAT='s/OPTIMIZE_FLAG = .*/OPTIMIZE_FLAG = ${OPTIMIZE_DEBUG_NO_WARN_FLAG}/'
+FAST_PAT='s/OPTIMIZE_FLAG = .*/OPTIMIZE_FLAG = ${OPTIMIZE_ON_FLAG}/'
+
+case "$LEVEL" in
+    "debug")
+        sed -i "$DEBUG_PAT" "$MAKEFILE_VARS"
+        ;;
+    "debug_no_warn")
+        sed -i "$DEBUG_NO_WARN_PAT" "$MAKEFILE_VARS"
+        ;;
+    "fast")
+        sed -i "$FAST_PAT" "$MAKEFILE_VARS"
+        ;;
+    *)
+        sed -i "$DEBUG_PAT" "$MAKEFILE_VARS"
+        ;;
+esac
+
+exit
